e4f38e4283a597010cd98c4c56ae35d3784c5e8f
Rodolfo Roman <rromanv@gmail.com>
2021-04-14 23:34:48 -0500

    Added NavBar and Auth

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,3 @@
 <template>
-  <div
-    class="min-h-screen flex justify-center items-center text-coolGray-300 font-extralight space-x-4 text-6xl"
-  >
-    <mdi:controller-classic-outline />
-    <h1 class="">Hello There</h1>
-  </div>
+  <NavBar />
 </template>
