8214d96c6bf9cfe8afe330c562c3751bf2a8b531
Rodolfo Roman <rromanv@gmail.com>
2021-04-15 02:12:29 -0500

    Added Messages

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,15 +1,44 @@
 <template>
-  <div class="container mx-auto flex bg-gray-700 my-4 rounded-full">
+  <div
+    class="container mx-auto flex my-4 rounded-full"
+    :class="isUser ? 'bg-green-800' : 'bg-gray-700'"
+  >
     <div>
-      <mdi:account class="bg-white rounded-full h-12 w-12" />
+      <img
+        class="rounded-full h-12"
+        :src="message.userPhotoURL"
+        :alt="`Avatar of ${message.userName}`"
+      />
     </div>
     <div class="flex flex-grow">
       <div
         class="flex flex-grow items-center justify-between mx-4 font-thin text-xl text-purple-200"
       >
-        <p>Message goes Here</p>
-        <p class="text-sm italic">Author goes here</p>
+        <p>{{ message.text }}</p>
+        <p class="text-sm italic">{{ message.userName }}</p>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+import { defineProps, computed, ref } from 'vue'
+import { authentication } from '~/helpers/useFirebase'
+
+const { user } = authentication()
+
+const isUser = computed(() => user.value.uid === props.message.userId)
+
+const props = defineProps({
+  message: {
+    type: Object,
+    default: () => ({
+      userName: '',
+      userId: '',
+      userPhotoURL: '',
+      text: '',
+      createdAt: '',
+    }),
+  },
+})
+</script>
--- src/components/NewMessage.vue
+++ src/components/NewMessage.vue
@@ -2,7 +2,30 @@
   <div
     class="absolute bottom-0 left-0 right-0 flex focus-within:ring-4 focus-within:ring-yellow-300"
   >
-    <input type="text" class="flex-grow text-xl font-thin focus:outline-none" />
-    <button class="bg-yellow-300 px-8 focus:outline-none"><mdi:send /></button>
+    <input
+      v-model="newMessage"
+      type="text"
+      class="flex-grow text-xl font-thin focus:outline-none"
+      @change="send"
+    />
+    <button class="bg-yellow-300 px-8 focus:outline-none" @click="send">
+      <mdi:send />
+    </button>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import { database } from '~/helpers/useFirebase'
+
+const { sendMessage } = database()
+
+const newMessage = ref(null)
+
+const send = () => {
+  if (newMessage.value?.length > 0) {
+    sendMessage(newMessage.value)
+    newMessage.value = null
+  }
+}
+</script>
